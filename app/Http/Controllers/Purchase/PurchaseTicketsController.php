3c31b0be560152b8c94b10af3a9873d13ac2a284
Ryuji Oda <ryuji.oda@arsaga.jp>
2020-10-14 12:01:01 +0900

    add Fee calc logic & judge type logic

---
+++ app/Http/Controllers/Purchase/PurchaseTicketsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Purchase;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PurchaseTicketsController extends Controller
+{
+    private int $ticket_fee = 3000;
+    private string $user_category = 'adult';
+
+    public function index () : ?string {
+        $user_category = new UserCategories($this->user_category);
+        $ticket_fee = new FeeCategories($this->ticket_fee);
+
+        if ($user_category->isAdult()) {
+            return $ticket_fee->getAdultFee();
+        }
+
+        if ($user_category->isSenior()) {
+            return $ticket_fee->getSeniorFee();
+        }
+
+        if ($user_category->isChild()) {
+            return $ticket_fee->getChildFee();
+        }
+        return null;
+    }
+}
