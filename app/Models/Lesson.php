e19531fc23fb08ddcaf3d400475638e9babea9c2
Ryuji Oda <ryuji.oda@gmail.com>
2020-11-25 10:02:33 +0900

    TDD refactor1: define getter for vaccancyLevel into lesson

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     use HasFactory;
+
+    /**
+     * @note 「getXxxxAttribute」という形式のメソッドを追加すれば、これがgetアクセサとして機能するようになっています。
+     * @return VacancyLevel
+     */
+    public function getVacancyLevelAttribute (): VacancyLevel
+    {
+        return new VacancyLevel($this->remainingCount());
+    }
+
+    private function remainingCount(): int
+    {
+        return 0;
+    }
 }
