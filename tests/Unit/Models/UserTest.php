6f5a1dd05adb922c9e7b9aafa80fafaf62d0bf3e
Ryuji Oda <ryuji.oda@gmail.com>
2020-12-01 10:04:11 +0900

    TDD: add UserTest

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function canReserve()
+    {
+        $user = new User();
+
+        
+    }
+}
