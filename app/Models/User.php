4f1438eaba3f0677e7a36b21a41b78d3016d5df8
Ryuji Oda <ryuji.oda@gmail.com>
2020-12-03 23:06:23 +0900

    TDD: add dataProvider

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    /**
+     * @var mixed|string
+     */
 
     public function lessons (): BelongsToMany
     {
--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -9,7 +9,7 @@ use function PHPUnit\Framework\assertTrue;
 class UserTest extends TestCase
 {
     /**
-     *
+     * @note
      *| パターン | レッスンの<br/>残り予約可能枠数 | ユーザーの<br/>プラン | ユーザーの<br/>当月予約数 | 予約の可否 |
      * |------|--------------------|---------------|-----------------|-------|
      * | 1    | > 1                | レギュラー         | < 5             | 可     |
@@ -17,40 +17,52 @@ class UserTest extends TestCase
      * | 3    | 0                  | レギュラー         | -               | 否     |
      * | 4    | > 1                | ゴールド          | -               | 可     |
      * | 5    | 0                  | ゴールド          | -               | 否     |
+     *
+     * @param string $plan
+     * @param int $remainingCount
+     * @param int|null $reservationCount
+     * @param bool $canReserve
+     * @dataProvider canReserveData
      */
-
-    public function testCanReserve()
+    public function testCanReserve(string $plan, int $remainingCount, int $reservationCount = null, bool $canReserve)
     {
         $user = new User();
+        
+        $user->plan = $plan;
+        assertSame($canReserve, $user->canReserve($remainingCount, $reservationCount));
+    }
 
-        // pattern 1
-        $user->plan = 'regular';
-        $remainingCount = 1;
-        $reservationCount = 4; //既存予約数
-        assertTrue($user->canReserve($remainingCount, $reservationCount));
-
-        // pattern 2
-        $user->plan = 'regular';
-        $remainingCount = 1;
-        $reservationCount = 5; //既存予約数
-        assertFalse($user->canReserve($remainingCount, $reservationCount));
-
-        // pattern 3
-        $user->plan = 'regular';
-        $remainingCount = 0;
-        assertFalse($user->canReserve($remainingCount));
-
-        // pattern 4
-        $user->plan = 'gold';
-        $remainingCount = 1;
-        $reservationCount = 4; //既存予約数
-        assertTrue($user->canReserve($remainingCount, $reservationCount));
-
-        // pattern 5
-        $user->plan = 'gold';
-        $remainingCount = 0;
-        $reservationCount = 4; //既存予約数
-        assertFalse($user->canReserve($remainingCount, $reservationCount));
+    public function canReserveData()
+    {
+        return [
+            '予約可能：残基あり、レギュラー、空きあり' => [
+                'plan' => 'regular',
+                'remainingCount' => 1,
+                'reservationCount' => 4,
+                'canReserve' => true,
+            ],
+            '予約不可：残基なし、レギュラー、空きあり' => [
+                'plan' => 'regular',
+                'remainingCount' => 1,
+                'reservationCount' => 5,
+                'canReserve' => false,
+            ],
+            '予約不可：レギュラー、空きなし' => [
+                'plan' => 'regular',
+                'remainingCount' => 0,
+                'canReserve' => false,
+            ],
+            '予約可能：ゴールド、空きあり' => [
+                'plan' => 'gold',
+                'remainingCount' => 1,
+                'canReserve' => true,
+            ],
+            '予約不可：ゴールド、空きなし' => [
+                'plan' => 'gold',
+                'remainingCount' => 0,
+                'canReserve' => false,
+            ],
+        ];
 
     }
 }
